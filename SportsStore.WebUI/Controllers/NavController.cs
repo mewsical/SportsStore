38785922f4e2b310db50dff6bd9ec57a89d5dd3a
mewsical <mildos_pastas@yahoo.com>
2017-09-10 13:17:44 +0300

    Mobile support.

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -23,7 +23,7 @@ namespace SportsStore.WebUI.Controllers
             .Select(x => x.Category)
             .Distinct()
             .OrderBy(x => x);
-            return PartialView(categories);
+            return PartialView("FlexMenu", categories);
         }
     }
 }
